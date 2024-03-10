6c6dbc4e904a7c4a777b47cba47847cdf168fbd3
IrinaNik05 <irinashik0510@gmail.com>
2024-03-10 19:50:11 +0300

    hometask-10

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const userName = document.querySelector("#name");
+const secondName = document.querySelector("#secondName");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const agree = document.querySelector("#agree");
 
-  // Здесь твой код
+function showMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.textContent = message;
+    document.body.append(div);
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    fetch(`https://polinashneider.space/user`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: IrinaNik05'
+            },
+            body: JSON.stringify({
+                "name": userName.value,
+                "secondName": secondName.value,
+                "phone": phone.value,
+                "email": email.value,
+                "agree": agree.checked,
+            }),
+        })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data);
+            showMessage('Данные успешно отправлены!');
+            form.reset();
+        })
+        .catch((error) => {
+            console.log(error);
+            showMessage('Ошибка отправки данных!');
+        })
+
 });
